a229d20d5e75cef4b8a70b7d9d84732c262cde1d
Piotr Czajka <piotr_czajka@outlook.com>
2019-03-13 21:33:14 +0100

    dfdsf

--- backend/kukuchat/core/models.py
+++ backend/kukuchat/core/models.py
@@ -1,10 +1,6 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 
-class Profile(models.Model):
-        user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
+class User(AbstractUser):
+    pass
