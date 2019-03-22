66346eb97c8de83b3f2c89b18afa9ec0fc1aa1ec
Maciek <mfmaciek666@gmail.com>
2019-03-22 14:03:52 +0100

    Added test_telegram class

---
+++ backend/kukuchat/core/providers_tests/test_telegram.py
@@ -0,0 +1,13 @@
+from unittest.mock import patch
+
+from django.test import TestCase
+
+from telethon import TelegramClient, sync
+
+from core.providers.telegram import TelegramProvider
+
+class TelegramTests(TestCase):
+
+    @patch.object(TelegramClient, 'TelegramClient')
+    def test_can_login(self, client_mock):
+        
