3ec553d3841b017fd74d845101c78b597733c5dc
llusaq <218406@student.pwr.edu.pl>
2019-04-05 15:07:08 +0200

    textinput prepared

--- frontend/kukuchat/src/components/Conversation.vue
+++ frontend/kukuchat/src/components/Conversation.vue
@@ -3,107 +3,241 @@
         <div class="topinfoboard">
             <div class="user_info">
                 <div class="whowith">Chat with Andro</div>
+                <div class="messagecount">78 Messages</div>
             </div>
-            <div class="messagecount">78 Messages</div>
+
+
         </div>
         <div class="messageboard">
             <div class="card">
                 <div class="holder">
 
                     <div class="msg_card_body">
-
                         <div class="msg_cont_area">
-                            <div class="msg_cotainer">
-                                Hi, how are you Andro?
-                                <span class="msg_time">8:40 AM, Today</span>
+
+                            <p class="text-secondary nomessages" v-if="messages.length==0">
+                                [No messages yet!]
+                            </p>
+
+                            <div class="messages">
+
+
+                                <div v-for="message in messages" :key="message.id">
+
+                                    <div v-if="message.name === 'me'">
+                                        <span class="text-info">[{{message.name}}]</span>
+
+
+                                        <div class="msg_cont_area">
+                                            <div class="msg_cotainer">
+                                                <span>{{message.message}}</span>
+
+                                                <span class="msg_time">{{message.timestamp}}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div v-else>
+
+                                        <span class="text-info">[{{message.name}}]</span>
+
+                                        <div class="msg_cont_send_area">
+                                            <div class="msg_cotainer_send">
+
+                                                <span>{{message.message}}</span>
+
+                                                <span class="msg_time_send">{{message.timestamp}}</span>
+                                            </div>
+                                        </div>
+
+
+                                    </div>
+
+
+                                </div>
 
                             </div>
-                        </div>
-                        <div class="msg_cont_send_area">
-                            <div class="msg_cotainer_send">
-                                Hi Andro i am good tnx how about you?
-                                <span class="msg_time_send">8:55 AM, Today</span>
-                            </div>
-                        </div>
 
-                        <div class="msg_cont_area">
-                            <div class="msg_cotainer">
-                                Hi, how are you Andro?
-                                <span class="msg_time">8:40 AM, Today</span>
+                            <div class="card-action">
+                                <CreateMessage :name="name"/>
+                            </div>
 
-                            </div>
                         </div>
-                        <div class="msg_cont_send_area">
-                            <div class="msg_cotainer_send">
-                                Hi Andro i am good tnx how about you?
-                                <span class="msg_time_send">8:55 AM, Today</span>
-                            </div>
-                        </div>
-
-                        <div class="msg_cont_area">
-                            <div class="msg_cotainer">
-                                Hi, how are you Andro?
-                                <span class="msg_time">8:40 AM, Today</span>
-
-                            </div>
-                        </div>
-                        <div class="msg_cont_send_area">
-                            <div class="msg_cotainer_send">
-                                Hi Andro i am good tnx how about you?
-                                <span class="msg_time_send">8:55 AM, Today</span>
-                            </div>
-                        </div>
-
-                        <div class="msg_cont_area">
-                            <div class="msg_cotainer">
-                                Hi, how are you Andro?
-                                <span class="msg_time">8:40 AM, Today</span>
-
-                            </div>
-                        </div>
-                        <div class="msg_cont_send_area">
-                            <div class="msg_cotainer_send">
-                                Hi Andro i am good tnx how about you?
-                                <span class="msg_time_send">8:55 AM, Today</span>
-                            </div>
-                        </div>
-                        <div class="msg_cont_area">
-                            <div class="msg_cotainer">
-                                Hi, how are you Andro?
-                                <span class="msg_time">8:40 AM, Today</span>
-
-                            </div>
-                        </div>
-                        <div class="msg_cont_send_area">
-                            <div class="msg_cotainer_send">
-                                Hi Andro i am good tnx how about you?
-                                <span class="msg_time_send">8:55 AM, Today</span>
-                            </div>
-                        </div>
-
 
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="textinputboard">
-            <textarea name="" class=" type_msg" placeholder="Type your message..."></textarea>
-            <div class="sendbtnarea">
-                <button class="btn waves-effect waves-light" type="submit" name="action">SEND
-                    <i class="material-icons right">send</i>
-                </button>
+
+            <!--
+            <div class="msg_cont_area">
+                <div class="msg_cotainer">
+                    Hi, how are you Andro?
+                    <span class="msg_time">8:40 AM, Today</span>
+
+                </div>
             </div>
+            <div class="msg_cont_send_area">
+                <div class="msg_cotainer_send">
+                    Hi Andro i am good tnx how about you?
+                    <span class="msg_time_send">8:55 AM, Today</span>
+                </div>
+            </div>
+
+            <div class="msg_cont_area">
+                <div class="msg_cotainer">
+                    Hi, how are you Andro?
+                    <span class="msg_time">8:40 AM, Today</span>
+
+                </div>
+            </div>
+            <div class="msg_cont_send_area">
+                <div class="msg_cotainer_send">
+                    Hi Andro i am good tnx how about you?
+                    <span class="msg_time_send">8:55 AM, Today</span>
+                </div>
+            </div>
+
+            <div class="msg_cont_area">
+                <div class="msg_cotainer">
+                    Hi, how are you Andro?
+                    <span class="msg_time">8:40 AM, Today</span>
+
+                </div>
+            </div>
+            <div class="msg_cont_send_area">
+                <div class="msg_cotainer_send">
+                    Hi Andro i am good tnx how about you?
+                    <span class="msg_time_send">8:55 AM, Today</span>
+                </div>
+            </div>
+
+            <div class="msg_cont_area">
+                <div class="msg_cotainer">
+                    Hi, how are you Andro?
+                    <span class="msg_time">8:40 AM, Today</span>
+
+                </div>
+            </div>
+            <div class="msg_cont_send_area">
+                <div class="msg_cotainer_send">
+                    Hi Andro i am good tnx how about you?
+                    <span class="msg_time_send">8:55 AM, Today</span>
+                </div>
+            </div>
+            <div class="msg_cont_area">
+                <div class="msg_cotainer">
+                    Hi, how are you Andro?
+                    <span class="msg_time">8:40 AM, Today</span>
+
+                </div>
+            </div>
+            <div class="msg_cont_send_area">
+                <div class="msg_cotainer_send">
+                    Hi Andro i am good tnx how about you?
+                    <span class="msg_time_send">8:55 AM, Today</span>
+                </div>
+            </div>
+
+
+            -->
+
+
+            <!-- <textarea name="" class=" type_msg" placeholder="Type your message..."></textarea>
+             <div class="sendbtnarea">
+                 <button class="btn waves-effect waves-light" type="submit" name="action">SEND
+                     <i class="material-icons right">send</i>
+                 </button>
+             </div>
+
+             -->
+
+            <div class="sendingsection">
+
+                <div class="row">
+                    <form class="col s12" rows="4" cols="50">
+                        <div class="row">
+
+
+                            <div class="input-field col s12" rows="4" cols="50">
+                                <textarea id="textarea1" class="materialize-textarea"></textarea>
+                                <label for="textarea1" rows="4" cols="50">Your message</label>
+
+                            </div>
+
+
+                        </div>
+                    </form>
+                </div>
+
+                <div class="sendbtnarea">
+                    <button @click="send()" class="btn waves-effect waves-light blue" type="submit" name="action">SEND
+                        <i class="material-icons right">send</i>
+                    </button>
+                </div>
+
+            </div>
+
+
         </div>
     </div>
+
+
 </template>
 
 <script>
     export default {
         name: 'conversation',
-        date() {
-            return {}
+
+
+        data() {
+
+
+            return {
+
+
+                messages: [
+                    {message: 'Hello!', timestamp: 'time: 2:34 date: 2.4.2019', name: 'me'},
+                    {message: 'Foo', timestamp: 'time: 2:35 date: 2.4.2019', name: 'Andro'},
+                    {message: 'Bar', timestamp: 'time: 2:36 date: 2.4.2019', name: 'me'},
+                    {message: 'Hello!', timestamp: 'time: 2:37 date: 2.4.2019', name: 'Andro'},
+                    {message: 'Foo', timestamp: 'time: 2:38 date: 2.4.2019', name: 'me'},
+                    {message: 'Bar', timestamp: 'time: 2:39 date: 2.4.2019', name: 'Andro'},
+                    {message: 'Blabla', timestamp: 'time: 2:44 date: 2.4.2019', name: 'Andro'},
+
+
+                ]
+
+            }
+        },
+
+        methods: {
+
+            send() {
+                var today = new Date();
+
+
+                var d = today.getDay();
+                var mo = today.getMonth();
+                var ye = today.getYear();
+
+                var h = today.getHours();
+                var m = today.getMinutes();
+
+                // this.messages.push({message:'my message',timestamp:"3:00",name:'me'});
+                this.messages.push({
+                    message: document.getElementById("textarea1").value,
+                    timestamp: 'time: ' + h + ':' + m + ' date: ' + d + '.' + (mo + 1) + '.' + (1900 + ye),
+                    name: 'me'
+                });
+                document.getElementById("textarea1").value = " ";
+
+
+            }
+
         }
+
+
     }
 </script>
 
@@ -124,7 +258,7 @@
         position: absolute;
         left: 0;
         bottom: -15px;
-        color: rgba(255, 255, 255, 0.5);
+        color: grey;
         font-size: 10px;
     }
 
@@ -137,7 +271,7 @@
     .card {
         height: 400px;
         border-radius: 15px !important;
-        background-color: rgba(0, 0, 0, 0.4) !important;
+
 
     }
 
@@ -151,6 +285,7 @@
 
         margin: 15px;
         float: right;
+        margin-bottom: 5px;
     }
 
     .msg_cotainer {
@@ -180,7 +315,7 @@
         position: absolute;
         right: 0;
         bottom: -15px;
-        color: rgba(255, 255, 255, 0.5);
+        color: grey;
         font-size: 10px;
     }
 
@@ -195,7 +330,7 @@
 
         height: auto;
         border-radius: 15px !important;
-        background: linear-gradient(black, grey);;
+        background: grey;;
 
 
     }
@@ -236,6 +371,22 @@
         color: white;
         margin-left: 10px;
     }
+
+    .sendingsection {
+        margin-top: 10px;
+        height: 100px;
+        border-radius: 15px !important;
+
+
+    }
+
+
+    .materialize-textarea {
+        max-height: 100px;
+        overflow: scroll;
+    }
+
+
 </style>
 
 
