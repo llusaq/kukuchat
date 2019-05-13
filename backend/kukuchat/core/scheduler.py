c4cb3bca2d8b49539aa0f83c5746944554f60cf2
Piotr Czajka <piotr_czajka@outlook.com>
2019-05-13 19:22:40 +0200

    sdfdsf

--- backend/kukuchat/core/scheduler.py
+++ backend/kukuchat/core/scheduler.py
@@ -22,15 +22,15 @@ class Scheduler:
             else:
                 return res
 
-    async def _get_messages_impl(self, chat_id):
-        func = functools.partial(self.consumer.get_messages, {'chat_ids': [chat_id]})
+    async def _get_messages_impl(self, chat_id, count):
+        func = functools.partial(self.consumer.get_messages, {'chat_ids': [chat_id], 'count': count})
         msgs = await self._try_few_times(func)
 
         await self.consumer.send_json({'action': 'new_message', **msgs})
 
     async def _get_messages(self, provider, data):
         for c_id in data['chat_ids']:
-            asyncio.create_task(self._get_messages_impl(c_id))
+            asyncio.create_task(self._get_messages_impl(c_id, data.get('count', 20)))
 
     async def add_task(self, data):
         method = f'_{data["method"]}'
