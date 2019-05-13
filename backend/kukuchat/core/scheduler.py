62ab25f0bb96fc4e1ec7f3a081971e5ca5be0171
Piotr Czajka <piotr_czajka@outlook.com>
2019-05-13 14:09:08 +0200

    sdfds

--- backend/kukuchat/core/scheduler.py
+++ backend/kukuchat/core/scheduler.py
@@ -1,12 +1,18 @@
+import asyncio
+
+
 class Scheduler:
 
     def __init__(self, consumer):
         self.consumer = consumer
 
+    async def _get_messages_impl(self, chat_id):
+        msgs = await self.consumer.get_messages({'chat_ids': [chat_id]})
+        await self.consumer.send_json(msgs)
+
     async def _get_messages(self, provider, data):
         for c_id in data['chat_ids']:
-            msgs = await self.consumer.get_messages({'chat_ids': [c_id]})
-            await self.consumer.send_json(msgs)
+            asyncio.create_task(self._get_messages_impl(c_id))
 
     async def add_task(self, data):
         method = f'_{data["method"]}'
